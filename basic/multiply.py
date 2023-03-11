bc2ad1d915aa2e419ac61ec9a27fde23794a778a
jongwhan <jonghwan@crypto.com>
2023-03-11 14:35:07 +0900

    multiply

--- basic/multiply.py
+++ basic/multiply.py
@@ -1,7 +1,9 @@
 import torch
 
-a=torch.rand(2,8,2,3)
-b=torch.rand(2,8,3,10)
+a=torch.rand(5,2,8,2,3)
+b=torch.rand(11,21,15,5,2,8,3,10)
 
 c=torch.matmul(a,b)
-print(c)
+print(a.shape)
+print(b.shape)
+print(c.shape)
